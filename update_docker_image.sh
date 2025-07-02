5faeb694338e112e0978c9bfbae7392d3e3095ad
tofu <tofu.connected@gmail.com>
2025-07-02 18:49:46 +0900

    fix docker build

--- update_docker_image.sh
+++ update_docker_image.sh
@@ -1,4 +1,7 @@
 docker pull tofuconnected/markov-chain-bot:latest
 docker stop markov-chain-bot
 docker rm markov-chain-bot
-docker run --name markov-chain-bot -d -p 8080:8080 --env TELEGRAM_BOT_TOKEN="YOUR_TELEGRAM_BOT_TOKEN" -v /volume1/Dev/markov-chain-bot/markov_data.db:/app/markov_data.db:rw tofuconnected/markov-chain-bot:latest
+docker run --name markov-chain-bot -d -p 8081:8080 \
+    --env TELEGRAM_BOT_TOKEN="7560852017:AAF3utJK0npPc-CWOPyylVkCTL6hoyVnQZU" \
+    -v /volume1/Dev/markov-chain-bot/markov_data.db:/app/markov_data.db:rw \
+    tofuconnected/markov-chain-bot:latest
