47e66185ae4a42f838e47ec1fa0086fc43097190
tofu <tofu.connected@gmail.com>
2025-08-09 21:19:17 +0900

    ebani menya palkoi

--- update_docker_image.sh
+++ update_docker_image.sh
@@ -2,6 +2,6 @@ docker pull tofuconnected/markov-chain-bot:latest
 docker stop markov-chain-bot
 docker rm markov-chain-bot
 docker run --name markov-chain-bot -d -p 8081:8080 \
-    --env TELEGRAM_BOT_TOKEN="7560852017:AAF3utJK0npPc-CWOPyylVkCTL6hoyVnQZU" \
+    --env TELEGRAM_BOT_TOKEN="YOUR_TOKEN_LOL" \
     -v /volume1/Dev/markov-chain-bot/markov_data.db:/app/markov_data.db:rw \
     tofuconnected/markov-chain-bot:latest
